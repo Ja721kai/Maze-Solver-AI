886b8a2cdebc3097c2be84f7bb49fe43c2099f09
Ja721kai <jan_kaiser69@outlook.de>
2019-09-26 16:45:40 +0200

    Implement optional constructor parameter for Cells

--- src/model/cell/Cell.py
+++ src/model/cell/Cell.py
@@ -3,5 +3,8 @@ from src.model.cell.ICell import ICell
 
 
 class Cell(ICell):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self, c_type=None):
+        if c_type is not None:
+            super().__init__(c_type)
+        else:
+            super().__init__()

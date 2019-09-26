205678b5e93f3a74286a170eaa574d81c8f5fc18
Jan Kaiser <jan_kaiser69@outlook.de>
2019-09-26 14:25:34 +0200

    Add an Agent Interface, a normal implementation and a mock implementation

---
+++ src/model/agent/Agent.py
@@ -0,0 +1,7 @@
+import numpy as np
+from src.model.agent.IAgent import IAgent
+
+
+class Agent(IAgent):
+    def __init__(self):
+        super().__init__()
---
+++ src/model/agent/IAgent.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# abstract base class for interface implementation
+import abc
+
+
+class IAgent(abc.ABC):
+    def __init__(self):
+        pass

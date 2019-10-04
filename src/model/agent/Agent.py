0da9868704ba9cab100ebf9d28085f7cb8ea1612
Jan Kaiser <jan.kaiser@htwg-konstanz.de>
2019-10-04 17:42:01 +0200

    Add basic Agent without being able to move or sense

--- src/model/agent/Agent.py
+++ src/model/agent/Agent.py
@@ -1,7 +1,14 @@
-import numpy as np
 from src.model.agent.IAgent import IAgent
 
 
 class Agent(IAgent):
     def __init__(self):
         super().__init__()
+
+    def set_state(self, state):
+        self.state = state
+
+
+
+
+
--- src/model/agent/IAgent.py
+++ src/model/agent/IAgent.py
@@ -5,5 +5,27 @@ import abc
 
 
 class IAgent(abc.ABC):
+
+    maze_map = []
+    state = None
+    V = []
+    R = []
+    policy = []
+
     def __init__(self):
         pass
+
+    def get_map(self):
+        return self.maze_map
+
+    def get_state(self):
+        return self.state
+
+    def get_value_table(self):
+        return self.V
+
+    def get_reward_table(self):
+        return self.R
+
+    def get_policy(self):
+        return self.policy
